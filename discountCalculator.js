d5a21c6c75a525ff0853c49c6dfa3727c1bd2000
Elvis Valenzuela <Elvis.valenzuela23@gmail.com>
2025-02-19 19:04:25 -0500

    changed the Value of i to 0

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,16 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger; // Pause execution at the start of the function
+    for (let i = 0; i < quantity; i++) { // Bug: Starts from 1 instead of 0
         totalPrice += pricePerItem;
     }
 
     if (quantity >= 10) {
-        totalPrice *= 0.9;
+        totalPrice *= 0.9; // Apply a 10% discount
     }
 
     return totalPrice;
 }
-
 
 
 module.exports = calculateDiscountedPrice;
